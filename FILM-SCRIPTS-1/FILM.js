3d9c00e81b840364dff4b5888bf7d46d834f96a8
Robert Dodson <rad@robdodson.net>
2020-10-09 13:09:29 -0700

    scripts

--- FILM-SCRIPTS-1/FILM.js
+++ FILM-SCRIPTS-1/FILM.js
@@ -501,14 +501,32 @@ addBlock(map)
 	clip:true,
 	textURL:"/Users/robertdodson/Desktop/FILM/FILM.js",
 	size:10,
-	textColor:gray,
 	font:MainFont,
-	//fillColor:gray,
-	strokeColor:orange,
+	strokeColor:cyan,
 	textColor:cyan,
 	animator0:{value:"scrollamount",amount:-5,min:-5000.0,max:0.0,type:INC},
 }
 addBlock(scrollblock)
+
+let scrollblock1 =
+{
+	name:"scrollblock1",
+	type:SCROLLTEXT,
+	x:20,
+	y:20,
+	layoutSpec:{x:4,y:2,fit:false},
+	width:400,
+	height:200,
+	radius:4,
+	clip:true,
+	textURL:"/Users/robertdodson/Documents/BeechwoodsNotes.txt",
+	size:10,
+	font:MainFont,
+	strokeColor:cyan,
+	textColor:blue,
+	animator0:{value:"scrollamount",amount:-7,min:-5000.0,max:0.0,type:INC},
+}
+addBlock(scrollblock1)
 
 let titleblock =
 {

d985f98b7d2d59fc7708a48534189b4e2494c0b4
Robert Dodson <rad@robdodson.net>
2020-10-11 13:14:11 -0700

    FLText working better!

--- FilmLang/FLText.swift
+++ FilmLang/FLText.swift
@@ -18,7 +18,9 @@ class FLText : Block
     var padding : CGFloat = 0.0
     var layer : CATextLayer!
     var gradientLayer : CAGradientLayer?
-    var rect : CGRect?
+    var backLayer : CALayer?
+    var rect : CGRect!
+    var backRect : CGRect!
     
     override func parseBlock(dict:NSDictionary)
     {
@@ -42,7 +44,7 @@ class FLText : Block
             layer = CATextLayer()
             
             let textStyle = NSMutableParagraphStyle()
-            textStyle.alignment = .left
+            textStyle.alignment = .center
             let textFontAttributes = [
                 .font: NSFont(name: font, size: size)!,
                 .foregroundColor: textColor,
@@ -52,30 +54,21 @@ class FLText : Block
                                                                   height: CGFloat.infinity),
                                                      options: .usesLineFragmentOrigin,
                                                      attributes: textFontAttributes)
-            
             rect = CGRect(x: 0,
                           y: 0,
                           width: textBoundingRect.width,
                           height: textBoundingRect.height)
             
-            layer.bounds = rect!
+            layer.bounds = rect
             layer.fontSize = size
             layer.font = CGFont(font as CFString)
             layer.foregroundColor = textColor.cgColor
-            layer.string = text
+            layer.string = NSAttributedString(string: text, attributes: textFontAttributes)
             
-            
-            if let strokecolor = strokeColor
-            {
-                layer.borderColor = strokecolor.cgColor
-                layer.borderWidth = strokeWidth
-                layer.cornerRadius = radius
-            }
-            
-            if let fillcolor = fillColor
-            {
-                layer.backgroundColor = fillcolor.cgColor
-            }
+            backRect = CGRect(x: 0 - padding,
+                                       y: 0 - padding,
+                                      width: textBoundingRect.width + (padding * 2),
+                                      height: textBoundingRect.height + (padding * 2))
             
             if let fillgradient = fillGradient
             {
@@ -83,7 +76,7 @@ class FLText : Block
                 
                 if let gradlayer = gradientLayer
                 {
-                    gradlayer.bounds = rect!
+                    gradlayer.bounds = backRect
                     
                     gradlayer.cornerRadius = radius
                     var color0 = NSColor()
@@ -92,20 +85,49 @@ class FLText : Block
                     fillgradient.getColor(&color1, location: nil, at: 1)
                     gradlayer.colors = [color0.cgColor,color1.cgColor]
                     
-                    Block.view.layer?.insertSublayer(gradlayer, below: layer)
+                    Block.view.layer?.addSublayer(gradlayer)
+                }
+            }
+            
+            if strokeColor != nil || fillColor != nil
+            {
+                backLayer = CALayer()
+                
+                if let backlayer = backLayer
+                {
+                    backlayer.bounds = backRect
+                    
+                    if let strokecolor = strokeColor
+                    {
+                        backlayer.borderColor = strokecolor.cgColor
+                        backlayer.borderWidth = strokeWidth
+                        backlayer.cornerRadius = radius
+                    }
+                    if let fillcolor = fillColor
+                    {
+                        backlayer.backgroundColor = fillcolor.cgColor
+                    }
+                    Block.view.layer?.addSublayer(backlayer)
                 }
             }
             
             Block.view.layer?.addSublayer(layer)
         }
         
-        let center = CGPoint(x: x + xoffset + (rect!.width / 2),
-                             y: y + yoffset + (rect!.height / 2))
+        let center = CGPoint(x: x + xoffset + (rect.width / 2),
+                             y: y + yoffset + (rect.height / 2))
+        
+        let backcenter = CGPoint(x: x + backRect.origin.x + xoffset + (backRect.width / 2),
+                                 y: y + backRect.origin.y + yoffset + (backRect.height / 2))
         
         layer.position = center
         if let gradlayer = gradientLayer
         {
-            gradlayer.position = center
+            gradlayer.position = backcenter
+        }
+        if let backlayer = backLayer
+        {
+            backlayer.position = backcenter
         }
   
         

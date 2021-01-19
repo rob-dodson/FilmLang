313fc1e7ed0e106b1364ccdeac45cd5de92ccdbd
Robert Dodson <rad@robdodson.net>
2021-01-19 10:47:06 -0800

    includeFile() now recursive

--- FilmLang/Javascript.swift
+++ FilmLang/Javascript.swift
@@ -67,54 +67,14 @@ class Javascript
             let folder = url.deletingLastPathComponent()
             let scripttorun = try String(contentsOf: url)
             
-            let lines = scripttorun.split(separator: "\n")
+            let fullscript = try handleInclude(folder: folder, filestring: scripttorun)
             
-            let deststring = NSMutableString.init()
-
-            for line in lines
-            {
-                if  line.contains("includeFile") == true
-                {
-                    let line_str = String(line)
-                    
-                    let re = try NSRegularExpression(pattern: #"includeFile\(\"(.*)\"\)"#, options: .caseInsensitive)
-                    let matches  = re.matches(in: line_str, options:[], range: NSMakeRange(0,line_str.count))
-                    
-                    let match = matches[0]
-                    if let filematch_range = line_str.rangeFromNSRange(nsRange: match.range(at: 1)) // capture is in 1
-                    {
-                        let includefile = String(line_str[filematch_range])
-                    
-                        var incpath : URL
-                        if !includefile.hasPrefix("/")
-                        {
-                            incpath = URL.init(fileURLWithPath:folder.absoluteString)
-                            incpath = incpath.appendingPathComponent(includefile)
-                        }
-                        else
-                        {
-                            incpath = URL.init(fileURLWithPath:includefile)
-                        }
-                        
-                        print("including: \(incpath)")
-                        
-                        let includefilestring = try String(contentsOf:incpath)
-                        deststring.append(includefilestring)
-                    }
-                }
-                else
-                {
-                    deststring.append(String(line))
-                }
-                
-                deststring.append("\n")
-            }
-            
+           
             
             //
             // execute script
             //
-            context?.evaluateScript(deststring as String)
+            context?.evaluateScript(fullscript)
         }
         catch
         {
@@ -132,3 +92,51 @@ class Javascript
 }
 
 
+func handleInclude(folder:URL, filestring:String) throws -> String
+{
+    let deststring = NSMutableString.init()
+    
+    let lines = filestring.split(separator: "\n")
+    for line in lines
+    {
+        if  line.contains("includeFile") == true
+        {
+            let line_str = String(line)
+            
+            let matches = try line_str.matchRegex(regex: #"includeFile\(\"(.*)\"\)"#)
+            
+            let match = matches[0]
+            if let filematch_range = line_str.rangeFromNSRange(nsRange: match.range(at: 1)) // capture is in 1, full match in 0
+            {
+                let includefile = String(line_str[filematch_range])
+            
+                var incpath : URL
+                if !includefile.hasPrefix("/")
+                {
+                    incpath = URL.init(fileURLWithPath:folder.absoluteString)
+                    incpath = incpath.appendingPathComponent(includefile)
+                }
+                else
+                {
+                    incpath = URL.init(fileURLWithPath:includefile)
+                }
+                
+                print("including: \(incpath)")
+                
+                var includefilestring = try String(contentsOf:incpath)
+                
+                includefilestring = try handleInclude(folder: incpath.deletingLastPathComponent(), filestring: includefilestring) // handle further includeFile() calls recursively
+                
+                deststring.append(includefilestring)
+            }
+        }
+        else
+        {
+            deststring.append(String(line))
+        }
+        
+        deststring.append("\n")
+    }
+    
+    return deststring as String
+}
